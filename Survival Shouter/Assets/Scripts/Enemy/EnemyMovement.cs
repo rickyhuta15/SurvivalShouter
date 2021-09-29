105fb60c66b5a2e606b607ea274fa7eff3863783
rickyhuta15 <rickyhutagalung89@gmail.com>
2021-09-29 19:31:52 +0700

    commit

---
+++ Survival Shouter/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform player;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
+    UnityEngine.AI.NavMeshAgent nav;
+
+
+   void Awake ()
+    {
+        //cari game object dengan tag player
+        player = GameObject.FindGameObjectWithTag ("Player").transform;
+
+        //get reference component
+        playerHealth = player.GetComponent <PlayerHealth> ();
+        enemyHealth = GetComponent <EnemyHealth> ();
+        nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+    }
+
+
+    void Update ()
+    {
+    
+       if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+      {
+            nav.SetDestination (player.position);
+       }
+        else
+        {
+          nav.enabled = false;
+        }
+    }
+}
